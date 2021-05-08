547c26b4e7ba404540e96e75d8a0977697a632ea
suheyla02 <hanimkiz451@gmail.com>
2021-05-08 05:51:16 +0200

    Q4

--- src/Solution04.java
+++ src/Solution04.java
@@ -14,14 +14,14 @@ public class Solution04 {
 
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        if(N>=20){
-            System.out.println("Not Weird");
-        }else if(N>6 || N<20){
+        if(N%2!=0){
             System.out.println("Weird");
-        }else if(N>2 || N<=5){
+        }else if(N>=6 && N<=20){
+            System.out.println("Weird");
+        }else if(N>=2 && N<=5){
             System.out.println("Not Weird");
-        }else {
-            System.out.println("Yanlis rakam girdiniz");
+        }else if(N>20){
+            System.out.println("Not Weird");
         }
 
         scanner.close();
